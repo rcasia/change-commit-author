622122ef962212a41136a60f50fab167c5f40b98
Ricardo Casía <ricardocasia.dev@gmail.com>
2023-04-29 20:09:02 +0200

    Added confirmation step

--- change-commit-author.sh
+++ change-commit-author.sh
@@ -1,11 +1,25 @@
 #!/bin/sh
 
+echo "\n"
 echo "Change authorship of commits"
 read -p "Enter your correct name: " NAME
 read -p "Enter your correct email address: " EMAIL
 read -p "Enter your old email address: " OLD_EMAIL
 
-git filter-branch --env-filter "
+echo "\n"
+echo "WARNING: This operation is potentially dangerous and can rewrite the history of your Git repository. Use with caution and make sure you have a backup of your repository before running this script."
+
+echo "\n"
+echo "$OLD_EMAIL ---> $EMAIL"
+echo "Are you sure you want to change the authorship of the commits? (y/n)"
+
+read -r CONFIRMATION
+if [ "$CONFIRMATION" != "y" ]; then
+    echo "Aborting operation."
+    exit 1
+fi
+
+output=$(git filter-branch --env-filter "
 if [ \"\$GIT_COMMITTER_EMAIL\" = \"$OLD_EMAIL\" ]
 then
     export GIT_COMMITTER_NAME=\"$NAME\"
@@ -16,7 +30,18 @@ then
     export GIT_AUTHOR_NAME=\"$NAME\"
     export GIT_AUTHOR_EMAIL=\"$EMAIL\"
 fi
-" --tag-name-filter cat -- --branches --tags
+" --tag-name-filter cat -- --branches --tags)
 
-echo "The authorship of the commits has been changed successfully. Please remember to force update on the remote repository."
-echo "Command: git push --force --tags origin 'refs/heads/*'"
+echo "\n"
+
+status=$?
+if [ $status -eq 0 -a "$output" != *"Cannot"* ];
+then
+  echo "Please remember to force update on the remote repository."
+  echo "Command: git push --force --tags origin 'refs/heads/*'"
+else
+  echo "Error occurred while changing authorship of commits."
+fi
+exit $status
+
+
